69894f25f8415c79c12d28bc15e66df02530da92
Pascal Nembot <36324527+NemsLucky@users.noreply.github.com>
2024-07-01 13:54:29 +0000

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'df8db8b67aa87508fcddd254335c3776aa74b51d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'df8db8b67aa87508fcddd254335c3776aa74b51d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.19.0',
+            'version' => '0.19.0.0',
+            'reference' => 'e95cd17be68e45f523cbfb0fe50cdd891b0cf20e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
